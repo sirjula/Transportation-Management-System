b9754d047e3a01a4873e773cd12c61e9e8a7727b
sirjula <sirjula875@gmail.com>
2024-11-14 17:27:48 +0545

    PHP

---
+++ home.php
@@ -0,0 +1,28 @@
+<style>
+    body {
+    background-image: url(./assets/img/bus9.png) ;
+    height: 100%;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+}
+    </style>
+<section id="" class="d-flex align-items-center">
+    <div class="container">
+    <center >
+      <br>
+    <h1 class="highlight" style= "text-align: left;">CITY TRAVELS</h1></center>
+      <?php if(!isset($_SESSION['login_id'])): ?>
+      	<center><button class="btn btn-danger btn-lg" type="button" id="book_now">Reserve Your Seats Now</button></center>
+      <?php else: ?>
+        
+		<center><br><br><br><h2 class="highlight2">Welcome, <?php echo $_SESSION['login_name'] ?></h2></center>
+	
+      <?php endif; ?>
+    </div>
+  </section>
+  <script>
+  	$('#book_now').click(function(){
+      uni_modal('Find Schedule','book_filter.php')
+  })
+  </script>
